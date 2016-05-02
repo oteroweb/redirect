3b7ff3f3b48f587aa95910ee777155efc3114548
pcimagenca <oterolopez1990@gmail.com>
2016-05-02 15:05:09 -0430

    añadido detalle en visualizacion

--- random.php
+++ random.php
@@ -5,9 +5,9 @@
 <?php 
 //zona de registro de anuncios entre comillas poner el codigo html
 $anuncios=array();
-$anuncios[]="<div> 	anuncio 1 </div>";
-$anuncios[]="<div>	anuncio2</div>";
-$anuncios[]="<div>	anuncio3</div>";
+$anuncios[]="<div style='background:blue' > 	anuncio 1 </div>";
+$anuncios[]="<div style='background:red'>	anuncio2</div>";
+$anuncios[]="<div style='background:green'>	anuncio3</div>";
 
  ?>
 
@@ -15,19 +15,22 @@ $anuncios[]="<div>	anuncio3</div>";
  <?php  
  //codigo para en este orden para mostrar el anuncio y eliminarlo esta funcion debe repetire tantas veces como exista el anuncio en este caso tres veces
  shuffle($anuncios); 
- end($anuncios);
+//instruccion especifica que muestr el anuncio
+echo end($anuncios);
   array_pop($anuncios);
  ?>
 
 
  <?php 
 shuffle($anuncios); 
- end($anuncios);
+ //instruccion especifica que muestr el anuncio
+ echo end($anuncios);
   array_pop($anuncios);
   ?>
 
  <?php  
  shuffle($anuncios); 
- end($anuncios);
+ //instruccion especifica que muestr el anuncio
+ echo end($anuncios);
   array_pop($anuncios);
   ?>
