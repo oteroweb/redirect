db93c87a68b5a26a7fca4ca841f3e2448ef5d9aa
pcimagenca <oterolopez1990@gmail.com>
2016-06-28 04:04:51 -0400

    status

--- nuevo.php
+++ nuevo.php
@@ -8,11 +8,33 @@
 
 
 <script>
-
+//debo cambiar
+// language
+// product
+// productSub
+// width
+// height
 //cambiar lenguaje con codigo
-// navigator.__defineGetter__('language', function(){
-    // return 'foo' // customized user agent
+// navigator.__defineGetter__('product', function(){
+//     return 'myproduct' // customized user agent
 // });
+// navigator.__defineGetter__('productSub', function(){
+//     return 'myproductSub' // customized user agent
+// });
+// navigator.__defineGetter__('language', function(){
+//     return 'mylanguage' // customized user agent
+// });
+// navigator.__defineGetter__('width', function(){
+//     return 'mywidth' // customized user agent
+// });
+// navigator.__defineGetter__('height', function(){
+//     return 'myheight' // customized user agent
+// });
+
+
+document.cookie = "language="+navigator.language;
+document.cookie = "width="+screen.width;
+document.cookie = "height="+screen.height;
 
 document.cookie = "appName="+navigator.appName;
 document.cookie = "appVersion="+navigator.appVersion;
@@ -20,12 +42,9 @@ document.cookie = "platform="+navigator.platform;
 document.cookie = "appCodeName="+navigator.appCodeName;
 document.cookie = "product="+navigator.product;
 document.cookie = "productSub="+navigator.productSub;
-document.cookie = "language="+navigator.language;
 document.cookie = "userAgent="+navigator.userAgent;
 document.cookie = "vendor="+navigator.vendor;
 document.cookie = "vendorSub="+navigator.vendorSub;
-document.cookie = "width="+screen.width;
-document.cookie = "height="+screen.height;
 document.cookie = "innerWidth="+window.innerWidth;
 document.cookie = "innerHeight="+window.innerHeight;
 document.cookie = "offsetWidth="+document.body.offsetWidth;
